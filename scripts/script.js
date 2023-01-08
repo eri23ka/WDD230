7a0210c12eba6ee639b40c0aa255fc47786e07d3
eri23ka <ramirezerika328@gmail.com>
2023-01-07 23:52:17 -0800

    live time/date

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,10 @@ const h1 = document.querySelector('h1');
 const copyright = document.querySelector('#copyright');
 
 const currentYear = new Date().getFullYear();
-copyright.textContent = `${currentYear}`;
+copyright.textContent = `©️${currentYear}`;
 
-try {
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-  };
-  document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
-} catch (e) {
-  alert("Error with code or browser does not support Locale");
-}
-
-
+ let text = document.lastModified;
+document.getElementById("time").innerHTML = text
 
 
 
