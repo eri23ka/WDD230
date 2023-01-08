f88f91bbbe8285adea73eb950afc9b466c097593
eri23ka <ramirezerika328@gmail.com>
2023-01-07 22:38:00 -0800

    update with folders

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
 const h1 = document.querySelector('h1');
 const copyright = document.querySelector('#copyright');
 
-h1.textContent = `Erika Ramirez Rendon`
 const currentYear = new Date().getFullYear();
-copyright.textContent = `©️ ${currentYear}`;
+copyright.textContent = `${currentYear}`;
+
+try {
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  };
+  document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+} catch (e) {
+  alert("Error with code or browser does not support Locale");
+}
+
+
+
+
+
+let quantity = document.querySelector('#q');
