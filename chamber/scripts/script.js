bd68c394da8bd413597f5defdaa9c79c25234ffb
eri23ka <ramirezerika328@gmail.com>
2023-02-04 23:00:48 -0800

    banner

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -4,3 +4,25 @@ lastModified.textContent = `${currentYear}`;
 document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
+
+
+
+/*-----Banner-------- */
+var banner1 = new Image;
+var banner2 = new Text;
+banner1.src = "frame.gif";
+banner2 = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+
+var frame = new Array(banner1, banner2);
+/*var delay = new Array(2500, 500, 2500); <--- It makes the body move*/
+var numframes = frame.length;
+var curframe = 0;
+
+function animateBanner(){
+		curframe++;
+		if(curframe == numframes) curframe = 0;
+		document.headerBanner.src = frame[curframe].src;
+		setTimeout("animateBanner()", 1200);
+		/*setTimeout("animateBanner()", delay[curframe]);*/
+
+}
